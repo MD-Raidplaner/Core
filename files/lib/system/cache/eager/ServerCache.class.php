025ba455f0a2aee249faebea2ea1313b63637ffc
xDerasier <deraiser@gmx.de>
2025-05-25 18:08:32 +0200

    Refactor server caching: replace ServerCacheBuilder with eager ServerCache implementation for improved performance and data handling

---
+++ files/lib/system/cache/eager/ServerCache.class.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace rp\system\cache\eager;
+
+use rp\data\server\ServerList;
+use rp\system\cache\eager\data\ServerCacheData;
+use wcf\system\cache\eager\AbstractEagerCache;
+
+/**
+ * Eager cache implementation for servers.
+ * 
+ * @author  Marco Daries
+ * @copyright   2025 MD-Raidplaner
+ * @license MD-Raidplaner is licensed under Creative Commons Attribution-ShareAlike 4.0 International 
+ * 
+ * @extends AbstractEagerCache<ServerCacheData>
+ */
+final class ServerCache extends AbstractEagerCache
+{
+    public function __construct(
+        private readonly int $gameID = \RP_CURRENT_GAME_ID
+    ) {}
+
+    #[\Override]
+    protected function getCacheData(): ServerCacheData
+    {
+        $serverList = new ServerList();
+        $serverList->getConditionBuilder()->add('gameID = ?', [$this->gameID]);
+        $serverList->readObjects();
+
+        $identifiers = [];
+        foreach ($serverList as $server) {
+            $identifiers[$server->identifier] = $server->getObjectID();
+        }
+
+        return new ServerCacheData(
+            $serverList->getObjects(),
+            $identifiers
+        );
+    }
+}
