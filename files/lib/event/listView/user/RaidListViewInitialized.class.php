7b5b8354b9cb8d6ae37082fb4c852e78cc633568
xDerasier <deraiser@gmx.de>
2025-05-30 08:15:38 +0200

    Add RaidListViewInitialized class to indicate initialization of the raid list view

---
+++ files/lib/event/listView/user/RaidListViewInitialized.class.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace rp\event\listView\user;
+
+use rp\system\listView\user\RaidListView;
+use wcf\event\IPsr14Event;
+
+/**
+ * Indicates that the raid list view has been initialized.
+ *
+ * @author  Marco Daries
+ * @copyright   2025 MD-Raidplaner
+ * @license MD-Raidplaner is licensed under Creative Commons Attribution-ShareAlike 4.0 International
+ */
+final class RaidListViewInitialized implements IPsr14Event
+{
+    public function __construct(public readonly RaidListView $listView) {}
+}
