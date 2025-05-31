594ffd91192638f4fe7c8d4364b37293a2c61dd1
xDerasier <deraiser@gmx.de>
2025-05-31 16:59:39 +0200

    Add __toString method

--- files/lib/system/game/GameItem.class.php
+++ files/lib/system/game/GameItem.class.php
@@ -2,6 +2,8 @@
 
 namespace rp\system\game;
 
+use wcf\system\WCF;
+
 /**
  * Represents a game item with a unique identifier.
  * 
@@ -14,4 +16,9 @@ final class GameItem
     public function __construct(
         public readonly string $identifier,
     ) {}
+
+    public function __toString()
+    {
+        return WCF::getLanguage()->get(\sprintf('rp.game.%s', $this->identifier));
+    }
 }
