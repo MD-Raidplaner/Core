80ffa0ebdfff2258289cd96bc51f1e86b0efeb4c
xDerasier <deraiser@gmx.de>
2025-06-01 09:13:10 +0200

    Refactor AvailableCharacter class to use race as a string and update related methods

--- files/lib/system/character/AvailableCharacter.class.php
+++ files/lib/system/character/AvailableCharacter.class.php
@@ -15,17 +15,11 @@ final class AvailableCharacter
 {
     /**
      * Constructor for the AvailableCharacter class.
-     *
-     * @param int|string $id        The identifier of the character. It can be an integer or a string.
-     * @param string $name      The name of the character.
-     * @param int|null $raceID      The race id of the character. It is optional and can be null.
-     * @param int|null $classificationID        The classification id of the character. It is optional and can be null.
-     * @param int|null $roleID      The role id of the character. It is optional and can be null.
      */
     public function __construct(
         private readonly int|string $id,
         private readonly string $name,
-        private readonly ?int $raceID = null,
+        private readonly ?string $race = null,
         private readonly ?int $classificationID = null
     ) {
     }
@@ -55,11 +49,11 @@ final class AvailableCharacter
     }
 
     /**
-     * Return the race id of the character.
+     * Return the race of the character.
      */
-    public function getRaceID(): ?int
+    public function getRace(): ?string
     {
-        return $this->raceID;
+        return $this->race;
     }
 
     public function __toString(): string
