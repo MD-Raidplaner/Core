8cfdbb05888c6258cff2b902e901c0f82749ac00
xDerasier <deraiser@gmx.de>
2025-06-01 07:06:51 +0200

    Remove obsolete faction classes and introduce FactionItem and FactionCollecting for improved faction management

---
+++ files/lib/event/faction/FactionCollecting.class.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace rp\event\faction;
+
+use InvalidArgumentException;
+use rp\system\faction\FactionItem;
+use wcf\event\IPsr14Event;
+
+/**
+ * Requests the collection of game items.
+ *
+ * @author  Marco Daries
+ * @copyright   2025 MD-Raidplaner
+ * @license MD-Raidplaner is licensed under Creative Commons Attribution-ShareAlike 4.0 International
+ */
+final class FactionCollecting implements IPsr14Event
+{
+    /**
+     * @var FactionItem[]
+     */
+    private array $factions = [];
+
+    /**
+     * Returns the registered factions.
+     *
+     * @return FactionItem[]
+     */
+    public function getFactions(): array
+    {
+        return $this->factions;
+    }
+
+    /**
+     * Registers a faction item.
+     */
+    public function register(FactionItem $faction): void
+    {
+        if (\array_key_exists($faction->identifier, $this->factions)) {
+            throw new \InvalidArgumentException(\sprintf(
+                'Faction with identifier %s already exists',
+                $faction->identifier
+            ));
+        }
+
+        $this->factions[$faction->identifier] = $faction;
+    }
+}
