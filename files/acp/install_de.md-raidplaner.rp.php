aa353d82d0dc84c10359cf916468ee88c53b9c80
xDerasier <deraiser@gmx.de>
2025-06-03 05:50:53 +0200

    Remove unused language handling code for default game setup

--- files/acp/install_de.md-raidplaner.rp.php
+++ files/acp/install_de.md-raidplaner.rp.php
@@ -1,38 +1,10 @@
 <?php
 
-use rp\system\game\GameItem;
-use wcf\data\language\item\LanguageItemEditor;
 use wcf\data\option\OptionEditor;
-use wcf\data\package\PackageCache;
-use wcf\system\language\LanguageFactory;
 use wcf\system\WCF;
 
 // set default game
 $game = 'default';
-
-$titles = [
-    'de' => 'Standard',
-    'en' => 'Default',
-];
-
-foreach ($titles as $languageCode => $value) {
-    $language = LanguageFactory::getInstance()->getLanguageByCode($languageCode);
-
-    $sql = "SELECT  languageCategoryID
-            FROM    wcf1_language_category
-            WHERE   languageCategory = ?";
-    $statement = WCF::getDB()->prepare($sql, 1);
-    $statement->execute(['rp.game']);
-    $languageCategoryID = $statement->fetchSingleColumn();
-
-    LanguageItemEditor::create([
-        'languageID' => $language->languageID,
-        'languageItem' => 'rp.game.default',
-        'languageItemValue' => $value,
-        'languageCategoryID' => $languageCategoryID,
-        'packageID' => PackageCache::getInstance()->getPackageByIdentifier('de.md-raidplaner.rp')->packageID,
-    ]);
-}
 
 // set default game to option
 $sql = "UPDATE  wcf1_option
