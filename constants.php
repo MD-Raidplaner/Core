9bb30009c97c213d4328f61ef624cb2a3b87fd4d
xDeraiser <info@daries.info>
2025-11-24 17:29:14 +0100

    Add constants.php file to define constants for IDE autocompletion

---
+++ constants.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Defines constants for autocompletion in IDEs. This file is not meant to be actively used anywhere!
+ * 
+ * @author  Marco Daries
+ * @copyright   2025 MD-Raidplaner
+ * @license MD-Raidplaner is licensed under Creative Commons Attribution-ShareAlike 4.0 International 
+ */
+
+// automatically defined constants
+define('RP_DIR', '');
+define('RELATIVE_RP_DIR', '');
+
+// option constants
