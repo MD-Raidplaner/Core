a635c60756185198cfe4e61d4d6ac964304425a1
xDeraiser <info@daries.info>
2025-12-27 15:00:49 +0100

    Add AbstractForm class for form handling and command execution

---
+++ files/lib/form/AbstractForm.class.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace rp\form;
+
+use wcf\form\AbstractFormBuilderForm;
+use wcf\system\request\LinkHandler;
+use wcf\system\WCF;
+
+/**
+ * Abstract implemation of a form.
+ * 
+ * @author  Marco Daries
+ * @copyright   2025 MD-Raidplaner
+ * @license MD-Raidplaner is licensed under Creative Commons Attribution-ShareAlike 4.0 International
+ */
+abstract class AbstractForm extends AbstractFormBuilderForm
+{
+    /**
+     * The command action to execute.
+     */
+    protected string $commandAction;
+
+    #[\Override]
+    public function save(): void
+    {
+        \wcf\form\AbstractForm::save();
+
+        $formData = $this->form->getData();
+
+        $data = $formData['data'] ?? [];
+        unset($formData['data']);
+        $data = \array_merge($this->additionalFields, $data);
+
+        if ($this->formAction === 'create') {
+            $command = new $this->commandAction($data, $formData);
+            $object = $command();
+        } else {
+            $command = new $this->commandAction($data, $formData, $this->formObject);
+            $command();
+        }
+
+        $this->saved();
+
+        WCF::getTPL()->assign('success', true);
+
+        if ($this->formAction === 'create' && $this->objectEditLinkController) {
+            WCF::getTPL()->assign(
+                'objectEditLink',
+                LinkHandler::getInstance()->getControllerLink($this->objectEditLinkController, [
+                    'id' => $object->getObjectID(),
+                ])
+            );
+        }
+    }
+}
